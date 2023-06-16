ddff4439b0a77782c4f04cc20595d4b6165ae976
Yannoush <dimbeya04@gmail.com>
2023-06-16 10:55:34 +0000

    my first size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,15 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{ 
+	printf("Size of a char: %t octects\n", sizeof(char));
+	printf("Size of int: %t octets \n", sizeof(int));
+	printf("Size of long int: %t octets \n", sizeof(long int));
+	printf("Size of long long int : %t octets \n", sizeof(long long int));
+	printf("Size of float : %t octets \n", sizeof(float));
+	return 0;
+}
+
