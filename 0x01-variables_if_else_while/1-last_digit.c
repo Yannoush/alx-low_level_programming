1a9d619b795ffb9cc1987edb900dfe090bbb8844
Yannoush <dimbeya04@gmail.com>
2023-06-17 00:08:25 +0000

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,9 +4,6 @@
 /**
  * main - main block
  * he output of the program should be:The string Last digit of, followed by n, followed by
- * the string is, followed by
- * if the last digit of n is greater than 5: the string and is greater than 5
- * if the last digit of n is 0: the string and is 0
  */
 int main(void)
 {
@@ -22,6 +19,6 @@ int main(void)
 	else if (last == 0)
 		printf("Last digit of %i is %i and is 0\n", n, last);
 	else if (last < 6)
-		printf("Last digit of %i is %i and less than 6 and noot 0\n", n, last);
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last);
 	return (0);
 }
