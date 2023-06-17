2b82b7e6a298f2350fd84c33a5fbc46db578cba8
Yannoush <dimbeya04@gmail.com>
2023-06-17 08:21:47 +0000

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * we supposed to create a function who print alphabet in lowercase and thn in uppercase
+ * Return: 0
+ */
+int main(void)
+{
+	char (c) = ('a')
+		char (d) = ('A')
+
+		while (c <= 'z')
+		{
+			putchar (c);
+			c++;
+		}
+	putchar ('\n');
+	else if ( d <= 'Z')
+		putchar (d);
+		d++;
+
+	return 0;
+}
