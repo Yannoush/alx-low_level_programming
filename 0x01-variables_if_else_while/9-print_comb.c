2430ba9258f2c1d9dc2a6c3cbcf92366bc7f0dfe
Yannoush <dimbeya04@gmail.com>
2023-06-17 22:06:42 +0000

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	int c;
-	for (c = 0; c <= 9; c++)
+	for (c = 0; c <= 9;)
 	{
 		putchar(c);
 		putchar(',');
@@ -16,6 +16,7 @@ int main(void)
 		{
 			putchar(c);
 		}
+		c++
 	}
 	putchar('\n');
 	return (0);
