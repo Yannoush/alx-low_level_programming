1bd42d2e9772a885c674ae05710b15bfb6b94ca9
Yannoush <dimbeya04@gmail.com>
2023-06-17 20:41:24 +0000

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - main block
+*print out number and letter
+*Return 0
+*/
+int main(void)
+{
+	char c;
+	
+	for ( c = '0'; c <= '0'; c++)
+	{
+		putchar(c);
+	}
+	for ( c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
