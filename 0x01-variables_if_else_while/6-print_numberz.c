4ee097389218b83d5c94d7c891e9cad578430a80
Yannoush <dimbeya04@gmail.com>
2023-06-17 11:40:04 +0000

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - main block
+ * we need to write a program 
+ * that print number 
+ * without using char
+ * Return: 0 
+ */
+int main(void)
+{
+	int c = '0';
+	while ( c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
