9d1bff94f63aa9eeeb20b2d372595cc1660af28e
Yannoush <dimbeya04@gmail.com>
2023-06-17 10:24:14 +0000

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * we must print all digit number
+ * from 0 to 10 
+ * Return 0
+ */
+int main(void)
+{
+	int digit = '10';
+
+	if (digit <= 10)
+		printf( c ; c++)
+			return (0);
+}
