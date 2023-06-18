8d65023600cf24f4e17b8f361c781ef7ea1d8b71
Yannoush <dimbeya04@gmail.com>
2023-06-18 11:57:10 +0000

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * print 3 number
+ * return: 0 
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c = 2;
+	while (c <=9)
+	{
+		a = 0; b = 1;
+		if (a != b; && b < a;)
+		{
+			putchar(48 + c);
+			putchar(48 + b);
+			putchar(48 + a);
+			if (a + b + c != 17)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		b++
+	}
+	c++
+		putchar('\n');
+	return (0);
+}
