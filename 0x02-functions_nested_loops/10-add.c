23d6eadd614134d5b3d7148420cea2ef48dfc5bc
Yannoush <dimbeya04@gmail.com>
2023-06-21 20:58:29 +0000

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds two integers a and b
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * Return: sum of a and b
+ * This function adds two integers and returns the result.
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,29 +7,15 @@
  */
 void jack_bauer(void)
 {
-	int a;
 		int b;
 		int c;
-		int d;
 
-		for (a = 0; a <= 2; a++)
+		for ( b = 0; b < 24; b++)
 		{
-			for (b = 0; b <= 9; b++)
-			{
-				for (c = 0;c <= 5; c++)
-				{
-					for (d = 0; d <= 9; d++)
-					{
-						if (a >= 2 && b >= 4)
-							break;
-						_putchar(a + 48);
-						_putchar(b + 48);
-						_putchar(58);
-						_putchar(c + 48);
-						_putchar(d + 48);
-						_putchar('\n');
-					}
-				}
-			}
+		       for (c = 0; c < 60; c++)
+		       {
+	       _putchar(c);
+		       }
+		       _putchar('\n');
 		}
-}
+}		
