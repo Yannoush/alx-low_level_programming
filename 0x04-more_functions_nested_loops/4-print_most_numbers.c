94557ab8d233447f8ace0f885d1bf6beeb2de759
Yannoush <dimbeya04@gmail.com>
2023-06-23 21:57:37 +0000

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Print all digits except 2 and 4
+ *
+ * This function prints all digits from 0-9 except for 2 and 4 to the console.
+ */
+
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		if (c != 50 && c != 52)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
