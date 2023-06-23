41d68b0ec7a95a9f7675d353e17e666135eb7ae0
Yannoush <dimbeya04@gmail.com>
2023-06-23 21:26:06 +0000

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print all digits
+ *
+ * This function prints all digits from 0-9 to the console.
+ */
+
+void print_numbers(void)
+{
+	char a;
+
+	for (a = 0; a < 10; a++)
+	{
+		_putchar(a);
+		_putchar('\n');
+	}
+}
