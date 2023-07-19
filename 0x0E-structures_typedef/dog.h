98f888714518db46d3f75d5607bae25278ed6400
Yannoush <dimbeya04@gmail.com>
2023-07-19 11:10:44 +0000

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
-#include <stdio.h>
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: longer
+ */
 struct dog
 {
 	char *name;
@@ -8,14 +16,15 @@ struct dog
 	char *owner;
 };
 
-struct dog *my_dog ( char *name, float age, char *owner)
-{
-struct dog *my_dog;
-my_dog = malloc(sizeof(struct dog));
-if (my_dog == NULL)
-	return (NULL);
-	my_dog.name = name;
-	my_dog.age = age;
-	my.dog.owner = owner;
-	return my_dog;
-}
+/**
+ * dog_t - typedef for struct dog
+ */
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
--- 0x0E-structures_typedef/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-Prototype: void init_dog(struct dog *d, char *name, float age, char *owner);
-Prototype: void print_dog(struct dog *d);
-Prototype: dog_t *new_dog(char *name, float age, char *owner);
-Prototype: void free_dog(dog_t *d);
-
-#endif
