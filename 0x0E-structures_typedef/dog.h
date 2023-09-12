66f1b01b7afbadf4800b49c163ae7009b8b63e91
Yannoush <dimbeya04@gmail.com>
2023-09-12 14:29:31 +0000

    0x0E. C - Structures, typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * _Base struct dog - is a basic info
+ * about dog
+ * @name: First structure member
+ * @age: Second structure member
+ * @owner: last structure member
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
